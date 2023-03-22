81d2e5481bac34c491e02ab718ac45e45bd4c1e2
BOOMER <127319193+golona73@users.noreply.github.com>
2023-03-22 13:42:21 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    String word = "Java";
+  for(int i=0; i< word.length() ;i++){
+
+    
+    System.out.println(word.charAt(i));
+  }
+  
+  
+  }
+
+  
+}
